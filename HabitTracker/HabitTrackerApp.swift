47d32127a6dcaec0cb0909b4db77e368a6137862
ZachMommaerts <zmommaerts@gmail.com>
2023-10-09 15:43:29 -0500

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Zach Mommaerts on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
